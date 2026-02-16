fa9ccf294ecf85390d95950b5d0bdf1a6f8beb21
almeldawy <almeldawy@gmail.com>
2026-02-16 10:59:50 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,24 @@
+{
+  "name": "MedAI Elite Concierge",
+  "short_name": "MedAI",
+  "description": "Assistant de diagnostic médical premium par Intelligence Artificielle",
+  "start_url": "/",
+  "display": "standalone",
+  "theme_color": "#7a3eb5",
+  "background_color": "#7a3eb5",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "/web-app-manifest-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "maskable"
+    },
+    {
+      "src": "/web-app-manifest-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable"
+    }
+  ]
+}
